ab1e204ef6cff396791a5b6b82ed6f876727744d
Dorofieieva <gergel.oly@gmail.com>
2024-01-22 20:02:29 +0200

    fix_3ex

--- ex3.py
+++ ex3.py
@@ -5,19 +5,23 @@ print('Ваш список: ', ", ".join(temp_product))
 # додавання видалення продуктів зі списку
 while len(temp_product) != 0:
     product_operation = input("Введіть ознаку (+/- для додавання/видалення) та продукт. Наприклад: +молоко або -хліб: ")
-    operation, product = product_operation[0], product_operation[1:].strip()
+    if len(product_operation) != 0:
+
+        operation, product = product_operation[0], product_operation[1:].strip()
     # додавання продукту
-    if operation == "+":
-        temp_product.append(product)
-        print(f'Оновлений список: {", ".join(temp_product)}')
+        if operation == "+":
+            temp_product.append(product)
+            print(f'Оновлений список: {", ".join(temp_product)}')
      # видалення продукту
-    elif operation == "-" and product in temp_product:
-        temp_product.remove(product)
-        print(f'Оновлений список: {", ".join(temp_product)}')
-    elif operation == "-" and product not in temp_product:
-        print("Цього продукту немає в переліку")
+        elif operation == "-" and product in temp_product:
+            temp_product.remove(product)
+            print(f'Оновлений список: {", ".join(temp_product)}')
+        elif operation == "-" and product not in temp_product:
+            print("Цього продукту немає в переліку")
+        else:
+            print("невірна ознака")
     else:
-        print("невірна ознака")
+        print("Помилка. Введіть валідні дані")
 print("не містить елементів. Програма завершена.")
 
 
