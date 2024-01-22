ac45f9f8595a0335c30bcbea6745d3776ab01479
Dorofieieva <gergel.oly@gmail.com>
2024-01-22 16:07:41 +0200

    fixed_ex3

--- ex3.py
+++ ex3.py
@@ -1,10 +1,7 @@
 # динамічний список покупок
 list_products = input("Введіть назви продуктів (через пробіл): ")
 temp_product = list_products.split()
-if not any(temp_product):
-    print("Ваш список порожній")
-else:
-    print(f"Ваш список: {temp_product}")
+print("Ваш список: ", ", ".join(temp_product))
 # додавання видалення продуктів зі списку
 while len(temp_product) != 0:
     product_operation = input("Введіть ознаку (+/- для додавання/видалення) та продукт. Наприклад: +молоко або -хліб: ")
@@ -12,11 +9,11 @@ while len(temp_product) != 0:
     # додавання продукту
     if operation == "+":
         temp_product.append(product)
-        print(f'Оновлений список: {" ".join(temp_product)}')
+        print(f'Оновлений список: {", ".join(temp_product)}')
      # видалення продукту
     elif operation == "-" and product in temp_product:
         temp_product.remove(product)
-        print(f'Оновлений список: {" ".join(temp_product)}')
+        print(f'Оновлений список: {", ".join(temp_product)}')
     elif operation == "-" and product not in temp_product:
         print("Цього продукту немає в переліку")
     else:
