07bc5dda2faf728175167b96960786093ab4fa74
Dorofieieva <gergel.oly@gmail.com>
2024-02-13 11:44:16 +0200

    update ex.7/1

--- ex.7.py
+++ ex.7.py
@@ -1,24 +1,25 @@
 print("Ex.1")
-def print_name_func_decorator(function):
-    def wrapper(num1, num2):
+def print_name_func_decorator(func):
+    def wrapper(*args, **kwargs):
         print("*" * 15)
-        print(f'Function name is {function.__name__}')
-        print(f'Result = {function(num1, num2)}')
-        print("*" * 15)
+        print(f'Function name is {func.__name__}')
+        return func(*args, **kwargs)
     return wrapper
 
 @print_name_func_decorator
 def my_sum(num1, num2):
-    print(f'You numbers is {num1} and {num2}')
-    return num2 + num1
+    return num1 + num2
 
 @print_name_func_decorator
 def my_multiple(num1, num2):
     print(f'You numbers is {num1} and {num2}')
-    return num2 * num1
+    return num1 * num2
 
-my_sum(33, 444)
-my_multiple(33, 444)
+if __name__ == '__main__':
+    result1 = my_sum(33, 444)
+    print(f'Сума двох чисел дорівнює', result1)
+    result2 = my_multiple(33, 444)
+    print(f'Частка двох чисел дорівнює', result2)
 
 print("Ex.2")
 # Створіть функцію, яка може друкувати квадрати парних чисел у діапазоні від 0 до 1000000000 включно
